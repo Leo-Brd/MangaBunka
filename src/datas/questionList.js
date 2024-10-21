65be71abb7659d0e958ea8793ea0287ea575449e
Leo-Brd <leobordet.pro@gmail.com>
2024-10-21 20:05:05 +0200

    add more questions

--- src/datas/questionList.js
+++ src/datas/questionList.js
@@ -25,84 +25,84 @@ export const questionList = [
         level: 1
 	},
     {
-		question: 'Quel mangaka est à l\'origine de "My Hero Academia" ?',
-        answer: 'Kohei Horikoshi',
-        badAnswer1: 'Eiichiro Oda',
-        badAnswer2: 'Kirei Hokashi',
-        badAnswer3: 'Mina Kagami',
+		question: 'Comment se nomme le personnage principal de Vagabond ?',
+        answer: 'Musashi',
+        badAnswer1: 'Momochi',
+        badAnswer2: 'Matahachi',
+        badAnswer3: 'Baiken',
+        level: 2
+	},
+    {
+		question: 'Comment se nomment les entités que chassent les exorcistes dans Jujutsu Kaisen ?',
+        answer: 'Les fléaux',
+        badAnswer1: 'Les fantômes',
+        badAnswer2: 'Les shinigamis',
+        badAnswer3: 'Les démons',
+        level: 2
+	},
+    {
+		question: 'Dans quel manga un personnage nommé Agni passe-t-il 8 ans à souffrir ?',
+        answer: 'Fire punch',
+        badAnswer1: 'Chainsaw man',
+        badAnswer2: 'Soul Eater',
+        badAnswer3: 'Kurenai',
+        level: 4
+	},
+    {
+		question: 'Comment se nomme le célèbre dragon exauceur de souhait dans Dragon Ball ?',
+        answer: 'Shenron',
+        badAnswer1: 'Rayquaza',
+        badAnswer2: 'Kai',
+        badAnswer3: 'Vegeta',
+        level: 1
+	},
+    {
+		question: 'Quand le légendaire auteur de Berserk, Kentaro Miura, est-il décédé ?',
+        answer: '6 Mai 2021',
+        badAnswer1: '14 Aout 2021',
+        badAnswer2: '3 Novembre 2021',
+        badAnswer3: '5 Fevrier 2022',
+        level: 5
+	},
+    {
+		question: 'Comment se nomme la forme démoniaque de Griffith en tant que God Hand ?',
+        answer: 'Femto',
+        badAnswer1: 'Conrad',
+        badAnswer2: 'Ubic',
+        badAnswer3: 'Void',
+        level: 2
+	},
+    {
+		question: 'En quelle année débute le manga One piece dans le Shonen Jump ?',
+        answer: '1997',
+        badAnswer1: '1998',
+        badAnswer2: '1999',
+        badAnswer3: '2000',
         level: 3
 	},
     {
-		question: 'Quel mangaka est à l\'origine de "My Hero Academia" ?',
-        answer: 'Kohei Horikoshi',
-        badAnswer1: 'Eiichiro Oda',
-        badAnswer2: 'Kirei Hokashi',
-        badAnswer3: 'Mina Kagami',
+		question: 'Qui représente le péché capital de l\'Envie dans Seven Deadly Sins ?',
+        answer: 'Diane',
+        badAnswer1: 'Merlin',
+        badAnswer2: 'Gowther',
+        badAnswer3: 'King',
         level: 3
 	},
     {
-		question: 'Quel mangaka est à l\'origine de "My Hero Academia" ?',
-        answer: 'Kohei Horikoshi',
-        badAnswer1: 'Eiichiro Oda',
-        badAnswer2: 'Kirei Hokashi',
-        badAnswer3: 'Mina Kagami',
-        level: 3
+		question: 'Quel arc précède Marine Ford dans One piece ?',
+        answer: 'Impel Down',
+        badAnswer1: 'Water Seven',
+        badAnswer2: 'L\'île des femmes',
+        badAnswer3: 'Dressrosa',
+        level: 2
 	},
     {
-		question: 'Quel mangaka est à l\'origine de "My Hero Academia" ?',
-        answer: 'Kohei Horikoshi',
-        badAnswer1: 'Eiichiro Oda',
-        badAnswer2: 'Kirei Hokashi',
-        badAnswer3: 'Mina Kagami',
-        level: 3
-	},
-    {
-		question: 'Quel mangaka est à l\'origine de "My Hero Academia" ?',
-        answer: 'Kohei Horikoshi',
-        badAnswer1: 'Eiichiro Oda',
-        badAnswer2: 'Kirei Hokashi',
-        badAnswer3: 'Mina Kagami',
-        level: 3
-	},
-    {
-		question: 'Quel mangaka est à l\'origine de "My Hero Academia" ?',
-        answer: 'Kohei Horikoshi',
-        badAnswer1: 'Eiichiro Oda',
-        badAnswer2: 'Kirei Hokashi',
-        badAnswer3: 'Mina Kagami',
-        level: 3
-	},
-    {
-		question: 'Quel mangaka est à l\'origine de "My Hero Academia" ?',
-        answer: 'Kohei Horikoshi',
-        badAnswer1: 'Eiichiro Oda',
-        badAnswer2: 'Kirei Hokashi',
-        badAnswer3: 'Mina Kagami',
-        level: 3
-	},
-    {
-		question: 'Quel mangaka est à l\'origine de "My Hero Academia" ?',
-        answer: 'Kohei Horikoshi',
-        badAnswer1: 'Eiichiro Oda',
-        badAnswer2: 'Kirei Hokashi',
-        badAnswer3: 'Mina Kagami',
-        level: 3
-	},
-    {
-		question: 'Quel mangaka est à l\'origine de "My Hero Academia" ?',
-        answer: 'Kohei Horikoshi',
-        badAnswer1: 'Eiichiro Oda',
-        badAnswer2: 'Kirei Hokashi',
-        badAnswer3: 'Mina Kagami',
-        level: 3
-	},
-    {
-		question: 'Quel mangaka est à l\'origine de "My Hero Academia" ?',
-        answer: 'Kohei Horikoshi',
-        badAnswer1: 'Eiichiro Oda',
-        badAnswer2: 'Kirei Hokashi',
-        badAnswer3: 'Mina Kagami',
-        level: 3
+		question: 'Quel manga a Poutine comme personnage principal ?',
+        answer: 'The ride-on king',
+        badAnswer1: 'The snow king',
+        badAnswer2: 'The beast king',
+        badAnswer3: 'The carry-on king',
+        level: 5
 	},
     {
 		question: 'Quel mangaka est à l\'origine de "My Hero Academia" ?',
