d1853d141b3e12659de99e81c99b2edf8bea51f6
Leo-Brd <leobordet.pro@gmail.com>
2025-01-30 14:41:47 +0100

    Add frontend

---
+++ frontend/src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home/home';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  </StrictMode>,
+);
---
+++ frontend/src/pages/home/home.jsx
@@ -0,0 +1,13 @@
+import './Home.scss';
+
+// The code for the home page component
+function Home() {   
+
+    return (
+        <main id="home">
+            <p>MangaBunka</p>
+        </main>
+    );
+}
+
+export default Home;
