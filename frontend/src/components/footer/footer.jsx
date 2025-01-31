56452c32ef0e71df77577851b91a3158cebd9ae4
Leo-Brd <leobordet.pro@gmail.com>
2025-02-01 00:17:51 +0100

    Footer v1

--- frontend/src/components/footer/footer.jsx
+++ frontend/src/components/footer/footer.jsx
@@ -1,9 +1,21 @@
 import './footer.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 export default function Footer() {
     return (
         <footer>
-            <p>footer</p>
+            <p>By Leo-Brd</p>
+
+            <a href="https://github.com/Leo-Brd">
+                <FontAwesomeIcon className="Header__icons" icon={faGithub} />
+            </a>
+
+            <a href="https://www.linkedin.com/in/leo-bordet/">
+                <FontAwesomeIcon className="Header__icons" icon={faLinkedin} />
+            </a>
+        
         </footer>
     )
 }
