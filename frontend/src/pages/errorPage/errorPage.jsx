44943d5e756a5a0c7d80c2fc63fb70633541ab55
Leo-Brd <leobordet.pro@gmail.com>
2025-02-01 15:04:07 +0100

    404page v1

--- frontend/src/pages/errorPage/errorPage.jsx
+++ frontend/src/pages/errorPage/errorPage.jsx
@@ -4,6 +4,7 @@ export default function ErrorPage() {
     return (
         <main id="error">
             <h1>404</h1>
+            <p>Je crois que vous vous êtes perdu...</p>
         </main>
     )
 }
