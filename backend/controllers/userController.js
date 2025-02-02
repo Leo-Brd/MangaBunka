9635e4d01173ba81dea488ec6d37a0656187abb5
Leo-Brd <leobordet.pro@gmail.com>
2025-02-02 17:10:26 +0100

    Add multer image to signup middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,28 +1,32 @@
 const User = require('../models/userModel');
 const bcrypt = require('bcrypt');
 
-// ALL THE FUNCTIONS FOR THE USERS
-
-// We create a new user with a hashed password
 exports.signup = async (req, res, next) => {
-    const { username, email, password, profilePic } = req.body;
+    try {
+        const { username, email, password } = req.body;
 
-    const existingUser = await User.findOne({ $or: [{ email }, { username }] });
-    if (existingUser) {
-        return res.status(400).json({ message: 'Email ou pseudo déjà utilisé.' });
+        const existingUser = await User.findOne({ $or: [{ email }, { username }] });
+        if (existingUser) {
+            return res.status(400).json({ message: 'Email ou pseudo déjà utilisé.' });
+        }
+
+        const profilePic = req.file
+            ? `/pp/${req.file.filename}`
+            : 'https://i.pravatar.cc/150?u=default';
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = new User({
+            username,
+            email,
+            password: hashedPassword,
+            profilePic,
+        });
+
+        await user.save();
+        res.status(201).json({ message: 'Utilisateur créé !' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Erreur lors de l\'inscription.' });
     }
-
-    bcrypt.hash(req.body.password, 10)
-        .then(hash => {
-            const user = new User({
-                username,
-                email,
-                password: hash,
-                profilePic: profilePic || undefined,
-            });
-            user.save()
-                .then(() => res.status(201).json({ message: 'Utilisateur créé !'}))
-                .catch(error => res.status(400).json({ error }))
-        })
-        .catch(error => res.status(500).json({ error }));
 };
